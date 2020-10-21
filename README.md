93da6ed12abb816ee8981afcdffe7cc2ed2dad1b
Matthew Denton <matt.denton@bonfyreapp.com>
2020-10-21 13:48:17 -0500

    initial working code for helloworld app

--- README.md
+++
@@ -1,2 +0,0 @@
-# spring-oracle-connector
-This is a simple skeleton backend that connects to oracle and dumps out stats.
---
+++ src/test/java/com/charter/ccpa/springoracleconnector/SpringOracleConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.charter.ccpa.springoracleconnector;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class SpringOracleConnectorApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
